2c27a3eca946b2616b77953ac428b83e519fb7e7
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-04-23 17:36:39 +0100

    setup sliding Divs

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+    $(".nextbutton").click(slideNextDiv);
+
+    function slideNextDiv() {
+        $("#basicleanslider_contents_wrapper").animate({
+            right: "500px",
+        });
+        $("#deepcleanslider_contents_wrapper").animate({
+            left: "0",
+        });
+    }
+
+    $(".previousbutton").click(slidePreviousDiv);
+
+    function slidePreviousDiv() {
+        $("#basicleanslider_contents_wrapper").animate({
+            left: "0",
+        });
+        $("#deepcleanslider_contents_wrapper").animate({
+            left: "500px",
+        });
+        
+
+    }
+})
