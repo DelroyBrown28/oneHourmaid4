c214e3415159cc9d5779ee9393e5a81ab7688156
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-04-23 20:43:09 +0100

    Updated active: for basic clean and deep clean // need to find a way to add 3rd div to wheel!

--- JS/script.js
+++ JS/script.js
@@ -1,32 +1,42 @@
 $(document).ready(function () {
 
 
-
     $(".nextbutton").click(slideNextDiv);
 
     function slideNextDiv() {
-        
         $("#basicleanslider_contents_wrapper").animate({
-            right: "500px",
+            left: "400px",
         });
-        $("#deepcleanslider_contents_wrapper").animate({
-            left: "0",
-        }).end();
-
-    };
-
-    $(".previousbutton").click(slidePreviousDiv);
-
-    function slidePreviousDiv() {
-        $("#basicleanslider_contents_wrapper").animate({
-            left: "0",
-        });
-        $("#deepcleanslider_contents_wrapper").animate({
-            left: "500px",
+        $("#top_contents_wrapper").animate({
+            left: "400px"
         })
-        
-        
+        $("#deepcleanslider_contents_wrapper").animate({
+            left: "0",
+        })
+        $("#deepClean_top_contents_wrapper").animate({
+            left: "0"
+        })
 
-    };
+
+    }
+
+    $(".previousbutton").click(previousDiv);
+
+    function previousDiv() {
+        $("#basicleanslider_contents_wrapper").animate({
+            left: "0",
+        });
+        $("#top_contents_wrapper").animate({
+            left: "0"
+        })
+        $("#deepcleanslider_contents_wrapper").animate({
+            left: "400px",
+        })
+        $("#deepClean_top_contents_wrapper").animate({
+            left: "400px"
+        })
+
+    }
+
 
 })
