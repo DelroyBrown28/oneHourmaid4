b57c78745946f73ba02f95a747476f772eb0ce62
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-04-28 11:46:37 +0100

    Working on contact form

---
+++ JS/sendEmail.js
@@ -0,0 +1,18 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "contact_form", {
+            "full_name": contactForm.fullname.value,
+            "user_email": contactForm.emailAddress.value,
+            "street_address": contactForm.streetAddress.value,
+            "town_city": contactForm.townCity.value,
+            "user_postcode ": contactForm.postcode.value,
+            "clean_request  ": contactForm.cleanRequest.value,
+        })
+        .then(
+            function (response) {
+                console.log("success", response);
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        )
+}
