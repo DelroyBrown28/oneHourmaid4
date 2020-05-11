2b337fe74e6dc22fb5ad09634fd0e9dfdc1e507a
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-05-11 16:59:02 +0100

    updated button sizes

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,10 +53,13 @@ $(document).ready(function () {
         $(".left_border, .right_border").delay(500).animate({
             width: "50px",
         }, 1000);
+        $("#chosen_cleaner, .cleaner_avatar__cib").animate({
+            opacity: "1",
+        },5000)
         $(".chosen_cleaner__wrap").animate({
-            borderLeftWidth: "8px",
-            borderRightWidth: "8px",
-        }, 1000)
+            borderLeftWidth: "4px",
+            borderRightWidth: "4px",
+        }, 500)
     }
     window.onload = addBorders();
 
@@ -125,4 +128,171 @@ function minusOneKitchens() {
     subtractRoom.setAttribute('value', value);
     subtractRoom.innerHTML = value;
 
+}
+
+var map, infoWindow;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        disableDefaultUI: true, //DISABLES GOOGLE MAPS DEFAULT FUNCTIONS
+        center: {
+            lat: -34.397,
+            lng: 150.644
+        },
+        zoom: 18,
+        disableDefaultUI: true, //DISABLES GOOGLE MAPS DEFAULT FUNCTIONS
+        styles: [{
+                elementType: 'geometry',
+                stylers: [{
+                    color: '#ffffff'
+                }]
+            },
+            {
+                elementType: 'labels.text.stroke',
+                stylers: [{
+                    color: 'transparent'
+                }]
+            },
+            {
+                elementType: 'labels.text.fill',
+                stylers: [{
+                    color: '#000000'
+                }]
+            },
+            {
+                featureType: 'administrative.locality',
+                elementType: 'labels.text.fill',
+                stylers: [{
+                    color: '#000000'
+                }]
+            },
+            {
+                featureType: 'poi',
+                elementType: 'labels.text.fill',
+                stylers: [{
+                    color: '#000000',
+                }]
+            },
+            {
+                featureType: 'poi.park',
+                elementType: 'geometry',
+                stylers: [{
+                    color: '#98A139'
+                }]
+            },
+            {
+                featureType: 'poi.park',
+                elementType: 'labels.text.fill',
+                stylers: [{
+                    color: '#228B22'
+                }]
+            },
+            {
+                featureType: 'road',
+                elementType: 'geometry',
+                stylers: [{
+                    color: '#EDC681'
+                }]
+            },
+            {
+                featureType: 'road',
+                elementType: 'geometry.stroke',
+                stylers: [{
+                    color: '#000000'
+                }]
+            },
+            {
+                featureType: 'road',
+                elementType: 'labels.text.fill',
+                stylers: [{
+                    color: '#000000',
+                }]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'geometry',
+                stylers: [{
+                    color: '#746855'
+                }]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'geometry.stroke',
+                stylers: [{
+                    color: '#1f2835'
+                }]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'labels.text.fill',
+                stylers: [{
+                    color: '#f3d19c'
+                }]
+            },
+            {
+                featureType: 'transit',
+                elementType: 'geometry',
+                stylers: [{
+                    color: '#2f3948'
+                }]
+            },
+            {
+                featureType: 'transit.station',
+                elementType: 'labels.text.fill',
+                stylers: [{
+                    color: '#d59563'
+                }]
+            },
+            {
+                featureType: 'water',
+                elementType: 'geometry',
+                stylers: [{
+                    color: '#318DB4'
+                }]
+            },
+            {
+                featureType: 'water',
+                elementType: 'labels.text.fill',
+                stylers: [{
+                    color: '#515c6d'
+                }]
+            },
+            {
+                featureType: 'water',
+                elementType: 'labels.text.stroke',
+                stylers: [{
+                    color: '#17263c'
+                }]
+            }
+        ]
+    });
+    infoWindow = new google.maps.InfoWindow;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
 }
